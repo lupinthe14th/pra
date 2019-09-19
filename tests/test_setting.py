addebb9d082c37e592106359584332712d887805
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-19 10:46:19 +0900

    feat: add testcase

---
+++ tests/test_setting.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from pra.setting import VERIFY
+
+
+def test_setting():
+    assert VERIFY == 1
+
+
+# vim: set fileencoding=utf-8 :
